3360b930e2b8ed326ff3b24b66ae3abb59b17249
BLACKWING7549 <69845256+BLACKWING7549@users.noreply.github.com>
2021-02-18 12:52:51 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,222 +1,149 @@
-# NOTE:
-# Don't use '-' or blank spaces in flag values! 
-# These will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC).
 #
-# NOTE-2:
-# all values within these brackets: "<" ">" showing choice values and need to be 
-# replaced by you with the correct values!
-# Example: if the codename of your device is "gtexslte" <device-codename> becomes:
-# SHRP_DEVICE_CODE := gtexslte
-# (so without any brackets ofc!)
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-################### ############################################
-# MANDATORY FLAGS # These flags HAVE TO be set/changed by you! #
-################### ############################################
+LOCAL_PATH := device/oppo/r9s
 
-# Device codename
-# Default (if not set): N/A
-SHRP_DEVICE_CODE := R9s
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 
-# Path of your SHRP device tree
-# Replace <device-brand> with the device brand name
-# (SHRP_DEVICE_CODE will expand to the above variable so check if that is correct)
-SHRP_PATH := device/oppo/$(SHRP_DEVICE_CODE)
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+TARGET_BOARD_SUFFIX := _64
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8953
+TARGET_NO_BOOTLOADER := true
+TARGET_PROVIDES_KEYMASTER := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
+TW_INCLUDE_CRYPTO_FBE := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+TW_CRYPTO_SYSTEM_VOLD_MOUNT := system vendor cust
+
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 58846064640 # (58846081024-16384)
+BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
+BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Recovery
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# System-as-root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 155
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_SUPERSU := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_IGNORE_MISC_WIPE_DATA := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TW_HAS_EDL_MODE := false
+TW_EXTRA_LANGUAGES := true
+ALLOW_MISSING_DEPENDENCIES := true
+TW_CRYPTO_SYSTEM_VOLD_MOUNT := system vendor cust
+AB_OTA_UPDATER := false
+TW_USE_TOOLBOX := true
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+TW_INCLUDE_NTFS_3G := true
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := R9s
+#SHRP
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/brand/codename
 # Maintainer name
-# Default (if not set): N/A
-SHRP_MAINTAINER := 憨憨wwy
-
-# Recovery Type (for "About" section only)
-# Default (if not set): N/A
-SHRP_REC_TYPE := Normal
-
-# Device Type (for "About" section only)
-# Default (if not set): N/A
-SHRP_DEVICE_TYPE := A_Only
-
-# Your device's recovery path, dont use blindly
-# No default
-SHRP_REC := </dev/block/bootdevice/by-name/recovery>
-
-################### ################################################################################
-# IMPORTANT FLAGS # These are usually good to check - at least if the defaults are what you expect #
-################### ################################################################################
-
-# Emergency DownLoad mode (0 = no EDL mode, 1 = EDL mode available)
-# Default (if not set): 0
-SHRP_EDL_MODE := <0|1>
-
-# internal storage path
-# Default (if not set): /sdcard
-SHRP_INTERNAL := /sdcard
-
-# If your device has an external sdcard
-# Default (if not set): /
+SHRP_MAINTAINER := QualityTeam
+# Device codename
+SHRP_DEVICE_CODE := c103
+# put this 0 if device has no EDL mode
+SHRP_EDL_MODE := 0
 SHRP_EXTERNAL := /external_sd
-
-# USB OTG path
-# Default (if not set): /
+SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usb_otg
-
-# Flashlight: (0 = disable, 1 = enable)
-# Default (if not set): 0
-SHRP_FLASH := <0|1>
-
-################## #########################################################################
-# OPTIONAL FLAGS # Stuff which highly depends on your device and / or personal preferences #
-################## #########################################################################
-
-# Use this flag only if your device is A/B.
-# Default (if not set) is no A/B device
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_AB := false
-
-# SHRP padding flag (for rounded corner devices only)
-# You have to change these values according to your device's roundness.
-SHRP_STATUSBAR_RIGHT_PADDING := <1-XXX>
-# Default (for LEFT): 20
-SHRP_STATUSBAR_LEFT_PADDING := <1-XXX>
-
-# For notch devices
-# Default (if not set) is no notch
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_NOTCH := true
-
-# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock
-# Default (if not set) is not using Express
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_EXPRESS := true
-
-# SHRP Dark mode, use this flag to have dark theme set by default
-# Default (if not set) is not using DARK mode
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_DARK := true
-
-# custom led paths for flashlight
-# find yours then replace the examples here
+# Put 0 to disable flashlight
+SHRP_FLASH := 1
+# These are led paths, find yours then put here (Optional)
 SHRP_CUSTOM_FLASHLIGHT := true
 SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
 SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
 SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
-
-# Max brightness of flashlight
-# you can also check the above led paths in Android when you turn on flashlight
-SHRP_FLASH_MAX_BRIGHTNESS := 200
-
-# Force mount system in /system despite SAR policy
-# useful for maintaining backwards compatibility and/or Samsung devices
-# Default (if not set) is to follow the SAR policy
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_NO_SAR_AUTOMOUNT := true
-
-# Do not include the SHRP theming system
-# Useful to save space for devices with a smaller recovery partition
-# Default (if not set) is full theming support
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_LITE := true
-
-################################## ##############################################
-# SHRP DEFAULT ADDONS - OPTIONAL # Default SHRP addon behavior - fully optional #
-################################## ##############################################
-
-# SHRP comes with a set of default addons.
-# This section allows to disable some or all of them, e.g. to save a little space
-# or when a device does not support / need them.
-
-#####
-# DEFAULT behavior if neither
-# - SHRP_SKIP_DEFAULT_ADDON_X nor
-# - INC_IN_REC_ADDON_X
-# are set:
-# the addon will be added to the build and saved into the internal storage
-# on flashing (i.e: $(SHRP_INTERNAL)/SHRP/addons)
-#
-# SHRP_SKIP_DEFAULT_ADDON_X := true
-# --> will not add this addon
-#
-# INC_IN_REC_ADDON_X := true
-# --> will add this addon & store it within the recovery ramdisk (i.e. NOT in the internal storage!)
-#
-# If SHRP_SKIP_DEFAULT_ADDON_X is set INC_IN_REC_ADDON_X will be ignored!
-#
-######
-
-# Addon - Substratum Overlay (OMS -Normal- disabler)
-# Default (if not set) is not skipping this addon (i.e. add it)
-# Ensure you understood the above note on the default behavior!
-SHRP_SKIP_DEFAULT_ADDON_1 := true
-# Default (if not set) is NOT adding it to the ramdisk but internal storage.
-# To store this addon into the recovery ramdisk instead set to "true" here.
-# Ensure you understood the above note on the default behavior!
-INC_IN_REC_ADDON_1 := true
-
-# Addon - Substratum Overlay (OMS -legacy- disabler)
-# Default (if not set) is not skipping this addon (i.e. add it)
-# Ensure you understood the above note on the default behavior!
-SHRP_SKIP_DEFAULT_ADDON_2 := true
-# Default (if not set) is NOT adding it to the ramdisk but internal storage.
-# To store this addon into the recovery ramdisk instead set to "true" here.
-# Ensure you understood the above note on the default behavior!
-INC_IN_REC_ADDON_2 := true
-
-# Addon - Clear Fingerprint (remove fingerprint lock from system)
-# Default (if not set) is not skipping this addon (i.e. add it)
-# Ensure you understood the above note on the default behavior!
-SHRP_SKIP_DEFAULT_ADDON_3 := true
-# Default (if not set) is NOT adding it to the ramdisk but internal storage.
-# To store this addon into the recovery ramdisk instead set to "true" here.
-# Ensure you understood the above note on the default behavior!
-INC_IN_REC_ADDON_3 := true
-
-# Addon - Force Encryption (remove force encryption from your device)
-# Default (if not set) is not skipping this addon (i.e. add it)
-# Ensure you understood the above note on the default behavior!
-SHRP_SKIP_DEFAULT_ADDON_4 := true
-# Default (if not set) is NOT adding it to the ramdisk but internal storage.
-# To store this addon into the recovery ramdisk instead set to "true" here.
-# Ensure you understood the above note on the default behavior!
-INC_IN_REC_ADDON_4 := true
-
-
-# Default (if not set) is NOT adding it to the ramdisk but internal storage.
-# To store magisk zip into the recovery ramdisk instead set to "true" here.
-# Ensure you understood the above note on the default behavior!
-INC_IN_REC_MAGISK := true
-
-############################ #########################################################
-# CUSTOM ADDONS - OPTIONAL # Custom addons! Yea fully optional but.. GREAT STUFF! :) #
-############################ #########################################################
-
-# SHRP can be extended as YOU wish! You can add whatever you can think of
-# e.g patching a ROM, adding stuff, apps, there is no limit ;)
-# Addons will be shown in the "Tweaks" section of SHRP.
-
-# Custom addon folder. Do not forget to put a "/" at the end of the path!
-SHRP_EXTERNAL_ADDON_PATH := "device/<device-brand>/$(SHRP_DEVICE_CODE)/<AddonFolderName>/"
-
-# Addon #1 - Name
-SHRP_EXTERNAL_ADDON_1_NAME := "LOS Recorder"
-# Addon #1 - Description
-SHRP_EXTERNAL_ADDON_1_INFO := "A magisk module which add lineageOS recorder into your system"
-# Addon #1 - Addon file name as ZIP (zip format is required)
-SHRP_EXTERNAL_ADDON_1_FILENAME := "los_recorder.zip"
-# Addon #1 - Free defineable button text the user need to press to actually install that addon
-# (Examples: Ok, Install, Flask, Enable, Disable, etc)
-SHRP_EXTERNAL_ADDON_1_BTN_TEXT := "Install"
-# Addon #1 - Text beeing shown when the installation was successful
-SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT := "Installed"
-# Addon #1 - Inject the addon into the recovery (if so: be sure that it will fit into the partition)
-# Default (if not set) is NOT adding this addon into the recovery ramdisk. That means:
-# If you do NOT set this the addon will be saved into the internal storage (i.e: $(SHRP_INTERNAL)/SHRP/addons)
-# Set this variable when true ONLY (do not use "false" or similiar)
-SHRP_INC_IN_REC_EXTERNAL_ADDON_1 := true
-
-# As you might already guess from the naming scheme: 
-# You can add multiple custom addons (max amount is 6)!
-#
-# just add the above flags again and replace:
-# SHRP_EXTERNAL_ADDON_1_XXXX
-# with
-# SHRP_EXTERNAL_ADDON_2_XXXX for the second addon
-# and for the third up to the sixth change it accordingly: 
-# SHRP_EXTERNAL_ADDON_3_XXXX, SHRP_EXTERNAL_ADDON_4_XXXX, SHRP_EXTERNAL_ADDON_5_XXXX, SHRP_EXTERNAL_ADDON_6_XXXX
+# Max Brightness of LED (Optional)
+SHRP_FLASH_MAX_BRIGHTNESS := 255
+# Check your device's recovery path, dont use blindly
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+# Use this flag only if your device is A/B
+SHRP_AB := false
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section]
+SHRP_REC_TYPE := Normal
+# Recovery Type (It can be A/B or A_only) [Only for About Section]
+SHRP_DEVICE_TYPE := A_Only
+# SHRP Padding Flag (Only for rounded corner devices.)
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock
+SHRP_EXPRESS := true
